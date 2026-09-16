5525712b154a38a3542f43ee87a1efd92a849784
phuonglinhday1048-hue <phuonglinhday.1048@gmail.com>
2026-09-16 17:21:29 +0700

    Update admin password from '123' to '9999'

--- auth.js
+++ auth.js
@@ -10,7 +10,7 @@ function login(username, password) {
   }
 
   // Kiểm tra tài khoản và mật khẩu chính xác
-  if (username === "admin" && password === "123") {
+  if (username === "admin" && password === "9999") {
     return true;
   }
 
